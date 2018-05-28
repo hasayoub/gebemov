84b4e7a1bbbc415e64dcfd6c35d22800dadcd5d0
hasayoub <37349438+hasayoub@users.noreply.github.com>
2018-05-28 14:45:34 +0100

    Update colorPick.js

--- colorPick.js
+++ colorPick.js
@@ -32,6 +32,7 @@
     $.fn.colorPick.defaults = {
         'initialColor': '#1b1b1b',
         'allowRecent': true,
+        'palette': ["#1abc9c", "#2ecc71", "#3498db", "#9b59b6", "#34495e", "#f1c40f", "#e67e22", "#e74c3c", "#ecf0f1", "#95a5a6", "#16a085",  "#27ae60", "#2980b9", "#8e44ad", "#2c3e50",  "#f39c12", "#d35400", "#c0392b", "#bdc3c7", "#7f8c8d"],	    
         'recentMax': 5,
         'onColorSelected': function() {
             this.element.css({'backgroundColor': this.color, 'color': this.color});
